09bcf4e0685c9fd66c43319b8e3e37fbb556afbc
Sebastian Medina <jtatan2014@gmail.com>
2023-09-10 23:28:11 -0500

    Toggle function added to calculator and image addition

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,17 +2,24 @@ const metricCircleContainer = document.getElementById('metricCircleContainer');
 const imperialCircleContainer = document.getElementById('imperialCircleContainer');
 const inputHeight = document.getElementById('HeightInputElement');
 const inputWeight = document.getElementById('WeightInputElement');
+const parametersMetric = document.querySelector('.parametersMetric');
+const parametersImperial = document.querySelector('.parametersImperial');
 
 metricCircleContainer.addEventListener('click', function () {
   metricCircleContainer.classList.add('active');
   imperialCircleContainer.classList.remove('active');
-  inputHeight.placeholder = 'cm';
-  inputWeight.placeholder = 'kg';
+  // inputHeight.placeholder = 'cm';
+  // inputWeight.placeholder = 'kg';
+  parametersImperial.classList.add('hidden');
+  parametersMetric.classList.remove('hidden');
 });
 
 imperialCircleContainer.addEventListener('click', function () {
   imperialCircleContainer.classList.add('active');
   metricCircleContainer.classList.remove('active');
-  inputHeight.placeholder = 'in';
-  inputWeight.placeholder = 'lbs';
+  // inputHeight.placeholder = 'in';
+  // inputWeight.placeholder = 'lbs';
+  parametersImperial.classList.remove('hidden');
+  parametersMetric.classList.add('hidden');
+  
 });
